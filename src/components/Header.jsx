dc8b6942844112c8ea0f983d4e3c20d6160b3f20
Emília Querino <emiliaquerino@gmail.com>
2024-03-05 17:26:41 -0300

    Remove elemento layout da função app

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <div className='logo'>Movie App</div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
