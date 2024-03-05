d7fd59dbf646a423fd898b7317a8b9af179aad9d
Emília Querino <emiliaquerino@gmail.com>
2024-03-05 17:55:03 -0300

    Altera cor de fundo

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Layout from './components/Layout';
 function App() {
   return (
     <div>
+      {/* <img src="https://m.media-amazon.com/images/M/MV5BMDdmZGU3NDQtY2E5My00ZTliLWIzOTUtMTY4ZGI1YjdiNjk3XkEyXkFqcGdeQXVyNTA4NzY1MzY@._V1_SX300.jpg" alt="random" /> */}
       <h1>TESTE</h1>
      {/* <Layout /> <Layout /> */}
     </div>
