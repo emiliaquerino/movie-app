d0d79f0d5799091154eac0025cdbc68aa6aab687
Emília Querino <emiliaquerino@gmail.com>
2024-03-06 09:25:04 -0300

    Cria componente MyFavorites

---
+++ src/pages/AllFilmes.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import "../styles/AllFilmes.css";
+
+const AllFilmes = () => {
+  //const
+
+  return (
+    <div>
+        <h1>All Filmes</h1>
+    </div>
+  );
+};
+
+export default AllFilmes;
---
+++ src/pages/MyFavorites.jsx
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import "../styles/MyFavorites.css";
+
+const MyFavorites = () => {
+  //const
+
+  return (
+    <div>
+        <h1>My Favorites</h1>
+    </div>
+  );
+};
+
+export default MyFavorites;
