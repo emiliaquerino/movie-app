23abfa160ec264c461e7894ee8a7ed23945d241c
Emília Querino <emiliaquerino@gmail.com>
2024-03-07 18:23:38 -0300

    Importa componente MovieCards para ser exibido na page AllFilms

--- src/pages/AllFilms.jsx
+++ src/pages/AllFilms.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from "react";
 import "../styles/AllFilms.css";
 import { MovieContext } from "../context/MovieContext";
+import MovieCard from "../components/MovieCard";
 
 const AllFilms = () => {
   const { fetchMovies, movies, totalResults } = useContext(MovieContext);
@@ -13,6 +14,9 @@ const AllFilms = () => {
     <div>
       <h1>All Films ({totalResults})</h1>
       <div className="movies">
+        {movies.map((movie) => (
+          <MovieCard key={movie.imdbID} movie={movie} />
+        ))}
       </div>
     </div>
   );
