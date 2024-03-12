8a2d9f5f9f14edebc9d78e13d49f201470c0a811
Emília Querino <emiliaquerino@gmail.com>
2024-03-12 15:28:57 -0300

    Modifica hierarquia de titulo em movieCard

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,7 +12,7 @@ const MovieCard = ({ movie }) => {
         <img src={movie.Poster} alt={`${movie.Title} poster`} />
       </div>
       <div className="movie-info">
-        <h1>{movie.Title}</h1>
+        <h2>{movie.Title}</h2>
         <div className="movie-metadata">
           <span className="movie-genre">{movie.Genre}</span>
           <span className="movie-rating">
