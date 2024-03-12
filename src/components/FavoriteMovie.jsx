23c56985147719c9b2483ae9b3f0e5293c709998
Emília Querino <emiliaquerino@gmail.com>
2024-03-12 11:48:18 -0300

    Refatora o componente FavoriteMovie

--- src/components/FavoriteMovie.jsx
+++ src/components/FavoriteMovie.jsx
@@ -1,36 +1,19 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { MovieContext } from '../context/MovieContext';
 
 const FavoriteMovie = ({ movie }) => {
-  const [isFavorite, setIsFavorite] = useState(false);
-
-  const checkFavorite = () => {
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    return favorites.some(favorite => favorite.imdbID === movie.imdbID);
-  };
-
-  const toggleFavorite = () => {
-    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    if (isFavorite) {
-      const updatedFavorites = favorites.filter(favorite => favorite.imdbID !== movie.imdbID);
-      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
-      setIsFavorite(false);
-    } else {
-      localStorage.setItem('favorites', JSON.stringify([...favorites, movie]));
-      setIsFavorite(true);
-    }
-  };
+  const { toggleFavorite, isFavorite } = useContext(MovieContext);
+  const [favorite, setFavorite] = useState(false);
 
   useEffect(() => {
-    setIsFavorite(checkFavorite());
-  }, []);
+    setFavorite(isFavorite(movie.imdbID));
+  }, [movie, isFavorite]);
 
   return (
-    <>
-      <Link onClick={toggleFavorite} className={isFavorite ? 'btn-black' : 'btn-white'}>
-        {isFavorite ? 'REMOVE FROM WATCHLIST' : 'ADD TO WATCHLIST'}
-      </Link>
-    </>
+    <Link onClick={() => toggleFavorite(movie)} className={favorite ? 'btn-black' : 'btn-white'}>
+      {favorite ? 'REMOVE FROM WATCHLIST' : 'ADD TO WATCHLIST'}
+    </Link>
   );
 };
 
