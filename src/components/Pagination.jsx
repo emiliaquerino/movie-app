6b122b835ba77ccdbe4adc1daea53731af736c97
Emília Querino <emiliaquerino@gmail.com>
2024-03-12 11:45:40 -0300

    Cria o componente de paginação

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Pagination = ({ currentPage, totalPages, onNext, onPrev }) => {
+  return (
+    <div className="pagination">
+      <button onClick={onPrev} disabled={currentPage === 1}>
+        Previous
+      </button>
+      <span> Page {currentPage} of {totalPages} </span>
+      <button onClick={onNext} disabled={currentPage === totalPages}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
