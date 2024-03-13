fcdea706cb9f06d4252e716334bf062cd6059d3e
Emília Querino <emiliaquerino@gmail.com>
2024-03-13 16:40:00 -0300

    Ajusta espaçamento  entre os elementos de movie rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import FavoriteMovie from "./FavoriteMovie";
-import ImdbLogo from "../assets/imdb-logo-home.png";
-import StarIcon from "../assets/star-icon.png";
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import FavoriteMovie from './FavoriteMovie';
+// import ImdbLogo from '../assets/imdb-logo-home.png';
+// import StarIcon from '../assets/star-icon.png';
+// import '../styles/MovieCard.css';
 
+// const MovieCard = ({ movie }) => {
+//   return (
+//     <div className="movie-card">
+//       <div className="movie-poster">
+//         <img src={movie.Poster} alt={`${movie.Title} poster`} />
+//       </div>
+//       <div className="movie-info">
+//         <h2>{movie.Title}</h2>
+//         <div className="movie-metadata">
+//           <div className="movie-genres">
+//             {movie.Genre.split(', ').map((genre, index) => (
+//               <button key={index} className="genre-button">
+//                 {genre}
+//               </button>
+//             ))}
+//           </div>
+//           <span className="movie-rating">
+//             <img src={ImdbLogo} alt="imdb logo" /> {movie.imdbRating}
+//             <img src={StarIcon} alt="star icon" />
+//           </span>
+//         </div>
+//         <p className="movie-description">{movie.Plot}</p>
+//         <div className="movie-actions">
+//           <FavoriteMovie movie={movie} />
+//           <Link to={`details/${movie.imdbID}`} className="btn-black">
+//             VIEW DETAILS
+//           </Link>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
-import "../styles/MovieCard.css";
+// export default MovieCard;
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import FavoriteMovie from './FavoriteMovie';
+import ImdbLogo from '../assets/imdb-logo-home.png';
+import StarIcon from '../assets/star-icon.png';
+import '../styles/MovieCard.css';
 
 const MovieCard = ({ movie }) => {
   return (
@@ -16,11 +56,20 @@ const MovieCard = ({ movie }) => {
       <div className="movie-info">
         <h2>{movie.Title}</h2>
         <div className="movie-metadata">
-          <span className="movie-genre">{movie.Genre}</span>
-          <span className="movie-rating">
-            <img src={ImdbLogo} alt="imdb logo" /> {movie.imdbRating}
-            <img src={StarIcon} alt="star icon" />
-          </span>
+          <div className="movie-genres">
+            {movie.Genre.split(', ').map((genre, index) => (
+              <button key={index} className="genre-button">
+                {genre}
+              </button>
+            ))}
+          </div>
+          <div className="movie-rating">
+            <img src={ImdbLogo} alt="imdb logo" />
+            <span>{movie.imdbRating}</span>
+            <div className="star">
+              <img src={StarIcon} alt="star icon"/>
+            </div>
+          </div>
         </div>
         <p className="movie-description">{movie.Plot}</p>
         <div className="movie-actions">
